a2d09fb764a754455d7b9f26826f6107e5790aaf
Doge_KingYT1 <dogekingyt1@gmail.com>
2024-02-16 18:02:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,8 @@ const songIDs = [
       const copyButton = document.createElement("button");
   
       songDiv.innerHTML = `<strong>${song.title}</strong>: ${song.id}`;
-      copyButton.textContent = "Copy <i class="fa-regular fa-clipboard"></i>";
+      copyButton.textContent = "Copy";
+      copyButton.innerHTML = `<i class="fa-regular fa-clipboard"></i>`
       copyButton.addEventListener("click", () => copyToClipboard(song.id));
   
       songDiv.appendChild(copyButton);
